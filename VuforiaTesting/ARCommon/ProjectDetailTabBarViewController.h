7a2c040b8da0715b5743555339875ad8be875e90
cmrodriguez25 <cmrodriguez25@me.com>
2013-08-03 18:56:59 -0400

    Tab Bar controller

---
+++ VuforiaTesting/ARCommon/ProjectDetailTabBarViewController.h
@@ -0,0 +1,14 @@
+//
+//  ProjectDetailTabBarViewController.h
+//  VuforiaTesting
+//
+//  Created by Michell on 8/3/13.
+//  Copyright (c) 2013 Michell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UIKit/UITabBar.h>
+
+@interface ProjectDetailTabBarViewController : UIViewController <UITabBarDelegate>
+
+@end
---
+++ VuforiaTesting/ARCommon/ProjectDetailTabBarViewController.m
@@ -0,0 +1,41 @@
+//
+//  ProjectDetailTabBarViewController.m
+//  VuforiaTesting
+//
+//  Created by Michell on 8/3/13.
+//  Copyright (c) 2013 Michell. All rights reserved.
+//
+
+#import "ProjectDetailTabBarViewController.h"
+#import <UIKit/UITabBar.h>
+
+@implementation ProjectDetailTabBarViewController
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [[UITabBar appearance] setBackgroundColor:[UIColor clearColor]];
+    [[UITabBar appearance] setBarTintColor:[UIColor clearColor]];
+    [[UITabBar appearance] setOpaque:NO];
+    
+	// Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
