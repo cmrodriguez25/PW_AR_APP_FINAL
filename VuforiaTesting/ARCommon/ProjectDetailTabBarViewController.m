3889e54254db5b27ac8a5d6034da2c02e0358ada
cmrodriguez25 <cmrodriguez25@me.com>
2013-08-06 11:17:02 -0400

    Committing changes to ProjectDetailTabController and related files

--- VuforiaTesting/ARCommon/ProjectDetailTabBarViewController.m
+++ VuforiaTesting/ARCommon/ProjectDetailTabBarViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ProjectDetailTabBarViewController.h"
-
+#import "QCARutils.h"
 
 
 @implementation ProjectDetailTabBarViewController
@@ -15,6 +15,45 @@
 -(void)prepareProjectDetail:(NSString *)projectName
 {
     _projectName = projectName;
+    
+    self.title = _projectName;
+    QCARutils *qUtils = [QCARutils getInstance];
+    
+    // Path to iPhone App Resources Folder
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    
+    // Path to the project specific Resources Folder
+    resourcePath = [NSString stringWithFormat:@"%@/Projects/%@", resourcePath, projectName];
+    
+    // First, get list of all targets needed for this project. These will be used
+    // by Vuforia to place models.
+    NSString *targets = [NSString stringWithFormat:@"%@/%@", resourcePath, @"Targets"];
+    NSError *error;
+    NSArray *directoryContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:targets error:&error];
+    
+    // For each xml target file in this directory, call Vuforia's "addTarget" method
+    for (NSString *item in directoryContents) {
+        if([item rangeOfString:@".xml"].location != NSNotFound) {
+            [qUtils addTargetName:[item stringByReplacingOccurrencesOfString:@".xml" withString:@""]
+                           atPath:[NSString stringWithFormat:@"%@/%@", targets, item]];
+        }
+    }
+    
+    // Load map image into appropriate UIImageView
+    NSString *mapImage = [NSString stringWithFormat:@"%@/Map", resourcePath];
+    directoryContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:mapImage error:&error];
+    UIViewController *viewController = self.viewControllers[0];
+    UIImageView *imgView = (UIImageView *)[viewController.view viewWithTag:100];
+    imgView.image = [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", mapImage, directoryContents[0]]];
+    
+    // Load project image into appropriate UIImageView
+    NSString *projectImage = [NSString stringWithFormat:@"%@/Picture", resourcePath];
+    directoryContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:projectImage error:&error];
+    viewController = self.viewControllers[1];
+    imgView = (UIImageView *)[viewController.view viewWithTag:100];
+    imgView.image = [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", projectImage, directoryContents[0]]];
+    
+    
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -33,7 +72,7 @@
     // DELETE: This is for testing purposes only.
     // prepareProjectDetail should be called by MainMenuViewController
     // with the project name once the user selects the correct project.
-    [self prepareProjectDetail:@"Project 1"];
+    [self prepareProjectDetail:@"Project1"];
     
     
     [[UITabBar appearance] setBackgroundColor:[UIColor clearColor]];
@@ -41,8 +80,6 @@
     
     self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
     self.navigationController.navigationBar.translucent = YES;
-    //[[UITabBar appearance] setOpaque:NO];
-    
 	// Do any additional setup after loading the view.
 
 }
