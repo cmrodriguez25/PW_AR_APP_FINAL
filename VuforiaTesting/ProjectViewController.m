8885c4122392d06362c1e0dfd0d3d968029ff83a
cmrodriguez25 <cmrodriguez25@me.com>
2013-09-13 15:59:46 -0400

    fdsaf

--- VuforiaTesting/ProjectViewController.m
+++ VuforiaTesting/ProjectViewController.m
@@ -14,6 +14,11 @@
 
 @implementation ProjectViewController
 
+
+- (void) popViewController {
+    [[self navigationController] popViewControllerAnimated:YES];
+}
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +32,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    
+    UIImage *backButtonImage = [UIImage imageNamed:@"backButton.png"];
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    [backButton setImage:backButtonImage
+                forState:UIControlStateNormal];
+    
+    backButton.frame = CGRectMake(0, 0, backButtonImage.size.width, backButtonImage.size.height);
+    
+    [backButton addTarget:self
+                   action:@selector(popViewController)
+         forControlEvents:UIControlEventTouchUpInside];
+    
+    UIBarButtonItem *backBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    self.navigationItem.leftBarButtonItem = backBarButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
